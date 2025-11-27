6fce40e9dd38c1a324ee8d9a05d9621793ac746d
Tomáš Landovský <landovsky@gmail.com>
2025-11-27 12:33:39 +0100

    Update hero section, introuce I18n

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "pages#home"
+  scope "(:locale)", locale: /en|cs/ do
+    root "pages#home"
+  end
 end
