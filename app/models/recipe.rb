4af48aaaef4f4e043925a308f343be17c5d023b9
Tomáš Landovský <landovsky@gmail.com>
2025-12-23 14:47:28 +0100

    Fix old serialize syntax

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :jam_session, optional: true
   belongs_to :author, optional: true
 
-  serialize :tags, Array
+  serialize :tags, type: Array, coder: YAML
 
   validates :title, presence: true
   validates :content, presence: true
